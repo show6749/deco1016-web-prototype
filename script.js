167ee14f9c19cc03f3e5e4644c16ed4053caebde
show6749 <show6749@users.noreply.github.com>
2019-11-19 17:36:47 +1100

    final v1

--- script.js
+++ script.js
@@ -40,30 +40,49 @@ var constraints = {
 }
 
 
-var form = document.querySelector("form");
+var form = document.querySelector(".form");
   form.addEventListener("submit", function(ev) {
     ev.preventDefault();
+
     handleFormSubmit(form);
 });
 
+
 function handleFormSubmit(form, input) {
     var errors = validate(form, constraints);
-    showErrors(form, errors || {});
-    if (!errors) {
-      showSuccess();
-    }
+    updateLocalStorage();
+    window.location.href = "memberhome.html";
+    // showErrors(form, errors || {});
+    // if (!errors) {
+    //   showSuccess();
+    // }
 }
 
 function showErrors(form, errors) {
     var errorPrint = document.getElementsByClassName("errors");
-
+    errorPrint.innerText = errors;
 }
 
 function addError(messages, error) {
     //create list of errors
+    var errorPrint = document.getElementsByClassName("errors");
+    errorPrint.innerHTML = errors;
 }
 
 function showSuccess() {
     //put stuff in local storage
     window.location.href = "memberhome.html";
+}
+
+function updateLocalStorage(){
+    localStorage.clear();
+    localStorage.setItem("0", document.getElementById("firstName").value);
+    localStorage.setItem("1", document.getElementById("lastName").value);
+    localStorage.setItem("2", document.getElementById("mobile").value);
+    localStorage.setItem("3", document.getElementById("email").value);
+    localStorage.setItem("4", document.getElementById("birthday").value);
+    localStorage.setItem("5", document.getElementById("cardNumber").value);
+    localStorage.setItem("6", document.getElementById("streetAddress").value);
+    localStorage.setItem("7", document.getElementById("state").value);
+    localStorage.setItem("8", document.getElementById("postcode").value);
 }
